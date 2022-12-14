d5c3aec33ede58eaaa39cdc28b538290276ecd32
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2022-12-14 09:51:10 -0500

    Create LunchDarkly.scala

---
+++ src/main/LunchDarkly.scala
@@ -0,0 +1,12 @@
+package com.lunchdarkly
+
+import java.lang.ClassNotFoundException
+
+
+object LunchDarkly {
+  def main(args: Array[String]) = {
+      println("Hello, world")
+  }
+  
+  def enabled(flag: String = throw ClassNotFoundException("error")
+}
